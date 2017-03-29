a1b1a0537ae4083903be4e240ff3c6e154a41fa4
Abhijit Annaldas <avannaldas@users.noreply.github.com>
2017-03-29 15:45:13 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='QuickView',
       version='0.1',
-      description='QuickView gives you a quick glance at the dataset, just pass the pandas dataframe. It gives some useful summary and plots with just one line of code. All the summary that it outputs is made available through member variables.',
+      description='QuickView gives you a quick glance at the dataset, just pass the pandas dataframe. It gives some useful summary and plots with just one line of code. All the summary that it outputs is made available through member variables. Its built using matplotlib.',
       url='http://github.com/avannaldas/QuickView',
       author='Abhijit Annaldas',
       author_email='avannaldas@hotmail.com',
